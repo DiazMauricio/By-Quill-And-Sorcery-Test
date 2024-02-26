772289bf2c51d9f9fa157382e08ed937a3e8d8b5
Diaz J. Mauricio <diazjmauricio95@gmail.com>
2024-02-26 19:00:52 -0300

    vite config: base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/by-quill-and-sorcery-test/",
+  base: "/By-Quill-And-Sorcery-Test/",
   plugins: [react()],
 })
